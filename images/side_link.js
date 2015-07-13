1e5573ce0cf1e247e620a212bc338c5f408f05a9
OPNay <opnayn@gmail.com>
2015-07-13 21:59:18 +0900

    sidebar: link: link_data move to skin_settings.js

--- images/side_link.js
+++ images/side_link.js
@@ -1,16 +1,6 @@
-var link_data =
-[
-/*	{"name": 이름, "fa": 아이콘 이름, "color": 링크 색상, "link":링크주소}, */
-/* 아이콘 이름은 http://fortawesome.github.io/Font-Awesome/icons 이곳을 참조하세요. */
-/* 색상은 RGB코드(#RRGGBB) 또는 CSS컬러 이름(black, white, gray)을 이용해주시면됩니다. */
-/* 각각 들어가는 글자들을 "로 꼭 묶어주세요. */
-	{"name":"Github", "fa":"github-alt", "color":"gray", "link":"https://github.com/OPNay"},
-	{"name":"RSS", "fa":"rss", "color":"orange", "link":rss_url},
-];
-
 $('#sidebar .module.link').ready(function () {
 	var side_link = $('.module.link'), side_link_style = side_link.find('style');
-	$.each(link_data, function (index, data) {
+	$.each(side_link_data, function (index, data) {
 		var link = $('<a>');
 		link.attr({
 				class: 's_l_' + data.name.toLowerCase(),
--- images/skin_settings.js
+++ images/skin_settings.js
@@ -1,2 +1,12 @@
 // 사이드바 네임 카드 설정
 var name_card_data = {"name":"OPNay", "href":"https://twitter.com/opnay_dev"};
+
+// 사이드바 링크
+var side_link_data = [
+//	{"name": 이름, "fa": 아이콘 이름, "color": 링크 색상, "link":링크주소},
+// 아이콘 이름은 http://fortawesome.github.io/Font-Awesome/icons 이곳을 참조하세요.
+// 색상은 RGB코드(#RRGGBB) 또는 CSS컬러 이름(black, white, gray)을 이용해주시면됩니다.
+// 각각 들어가는 글자들을 "로 꼭 묶어주세요.
+	{"name":"Github", "fa":"github-alt", "color":"gray", "link":"https://github.com/OPNay"},
+	{"name":"RSS", "fa":"rss", "color":"orange", "link":rss_url},
+];
