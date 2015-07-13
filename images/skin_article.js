6f87e62114e4f785b08bb0fcb8b4bc35b26ba2c1
OPNay <opnayn@gmail.com>
2015-07-13 22:59:43 +0900

    entry: protected: Update protected article. * add password input. * show article title and info.

--- images/skin_article.js
+++ images/skin_article.js
@@ -1,3 +1,7 @@
+$('#entry_protected').ready(function () {
+    $(this).find('h2').text($('#entry_protected h2').text().replace(/\ 게시글입니다./g,''));
+});
+
 $('.article').ready(function () {
     // Image
     var img_block = $(this).find('.imageblock');
