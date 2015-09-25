03e1ff6a5317cd904a36199fe34864df3d095128
Inseop Kim <opnayn@gmail.com>
2015-09-26 03:26:48 +0900

    js: Update article img styles.

--- images/skin_article.js
+++ images/skin_article.js
@@ -1,11 +1,9 @@
 $('#entry_protected').ready(function () {
     $('#entry_protected h2').text($('#entry_protected h2').text().replace(/\ 게시글입니다./g,''));
 });
-
 $('.article').ready(function () {
     // Image
     var img_block = $(this).find('.imageblock');
     img_block.removeAttr('style');
-    img_block.find('img').removeAttr('height');
-    img_block.find('img').removeAttr('width');
+    img_block.find('.cap1').attr('style','display: block;');
 });
