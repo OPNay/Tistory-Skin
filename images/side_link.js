25d810901afc435e9c8812fc0897a8e4078c1f30
Inseop Kim <opnayn@gmail.com>
2015-10-16 22:21:51 +0900

    side: link: Update links style.

--- images/side_link.js
+++ images/side_link.js
@@ -1,5 +1,5 @@
 $('#sidebar .module.link').ready(function () {
-	var side_link = $('.module.link'), side_link_style = side_link.find('style');
+	var side_link = $('.module.link'), side_link_style = $('<style>');
 	$.each(side_link_data, function (index, data) {
 		var link = $('<a>');
 		link.attr({
@@ -11,5 +11,6 @@ $('#sidebar .module.link').ready(function () {
 		
 		side_link_style.append('#sidebar .link .s_l_' + data.name.toLowerCase() + ' {color:' + data.color + ';}\n');
 		side_link_style.append('#sidebar .link .s_l_' + data.name.toLowerCase() + ':hover {background-color:' + data.color + ';}\n');
+		side_link.append(side_link_style);
 	});
 });
