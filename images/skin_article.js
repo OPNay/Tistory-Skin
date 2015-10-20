05747da369f556f6c38c77e17f559f1f20ae5d34
Inseop Kim <opnayn@gmail.com>
2015-10-20 19:34:56 +0900

    js: skin_article: Cleanup unusedcode.

--- images/skin_article.js
+++ images/skin_article.js
@@ -1,6 +1,3 @@
-$('#entry_protected').ready(function () {
-    $('#entry_protected h2').text($('#entry_protected h2').text().replace(/\ 게시글입니다./g,''));
-});
 $('.article').ready(function () {
     // Image
     var img_block = $(this).find('.imageblock');
