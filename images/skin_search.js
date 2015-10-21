c28d9edb5de08af00a1c15a1f7a309d3deb65358
Inseop Kim <opnayn@gmail.com>
2015-10-21 20:11:52 +0900

    search: Update search method. - When current page is search, category, archive page,   move to search page. - or not, Add search result after search card.

--- images/skin_search.js
+++ images/skin_search.js
@@ -1,8 +1,27 @@
 function t_search() {
-	var query = $('#query').val();
-	(query != '') && $(location).attr('href', blog_link + '/search/' + query);
+	var query = $('#query').val(),
+		url = blog_link + '/search/' + query;
+	var pathname = decodeURI($(location).attr('pathname').replace(/^\//,'')).split('/')[0];
+	
+	if(query == '')
+		return;
+
+	if ((pathname == 'search') || (pathname == 'category') || (pathname == 'archive'))
+		$(location).attr('href',url);
+	else {
+		$.ajax({
+			url: url,
+			success: function(data, textStatus, jqXHR) {
+				var list = $(data).find('#search_list, #page');
+				$('#search_list, #search_list + #page').remove();
+				if(list.find('.card-list a').length > 0)
+					$('#search').after(list.clone());
+			}
+		});
+	}
 }
-$(document).ready(function () {
+
+$('#search').ready(function () {
 	$('#query').keypress(function (e) {(e.keyCode == 13) && t_search();});
 	$('#query ~ .button').click(t_search);
 });
