7f9b93b7a6a05319eaf71fc60e572180feb4e11a
Inseop Kim <opnayn@gmail.com>
2015-10-23 00:52:58 +0900

    js: skin_side: Move to skin.js

--- images/skin.js
+++ images/skin.js
@@ -117,3 +117,24 @@ search.ready(function () {
 	$('#query').keypress(function (e) {(e.keyCode == 13) && t_search();});
 	$('#query ~ .button').click(t_search);
 });
+
+function highlightSide() {
+	var pathname = decodeURI($(location).attr('pathname').replace(/^\//,'')).split('/');
+	switch(pathname[0]) {
+		case 'category':
+			if(pathname [1]) {
+			var name = pathname[2] ? pathname[2] : pathname[1];
+			$('#side a').each(function (index) {
+				if(name == $(this).text().replace(/\s\(\d+\)/g,''))
+					$(this).addClass('accent');
+			});
+			} else
+				$('#side a[href="/category"]').addClass('accent');
+			break;
+		default:
+			$('#side a[href="/'+pathname[0]+'"]').addClass('accent');
+			break;
+	}
+}
+
+$('#side').ready(highlightSide);
