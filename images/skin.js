f8c97f608ed1f138a69fa2e153fea0d10da7b87f
Inseop Kim <opnayn@gmail.com>
2015-10-23 00:49:01 +0900

    js: skin_search: Move to skin.js

--- images/skin.js
+++ images/skin.js
@@ -1,6 +1,7 @@
 var body = $('body'),
 	cover = $('#cover'),
 	nav = $('#nav'),
+	search = $('#search'),
 	page = $('#page'),
 	botbtn = $('#bot-btn');
 
@@ -85,4 +86,34 @@ botbtn.ready(function () {
 	b_arrow_up.click(function () {$('html, body').animate({'scrollTop':'0'}, 250); return false;});
 	$(window).scroll(buttom_btn_toggle);
 	buttom_btn_toggle();
+});
+
+function t_search() {
+	var query = $('#query').val(),
+		url = blog_link + '/search/' + query;
+	var pathname = decodeURI($(location).attr('pathname').replace(/^\//,'')).split('/')[0];
+	
+	if(query == '')
+		return;
+
+	if ((pathname == 'search') || (pathname == 'category') || (pathname == 'archive'))
+		$(location).attr('href',url);
+	else {
+		$.ajax({
+			url: url,
+			success: function(data, textStatus, jqXHR) {
+				var list = $(data).find('#search_list, #page').clone();
+				$('#search_list, #search_list + #page').remove();
+				f_page(list.filter('#page'));
+				if(list.find('.card-list a').length > 0) {
+					search.after(list);
+				}
+			}
+		});
+	}
+}
+
+search.ready(function () {
+	$('#query').keypress(function (e) {(e.keyCode == 13) && t_search();});
+	$('#query ~ .button').click(t_search);
 });
