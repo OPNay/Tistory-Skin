3fa522ee8f229bd7716d9bad1c9429a953f039f3
Inseop Kim <opnayn@gmail.com>
2015-11-14 21:26:43 +0900

    js: Rewrite skin.js - Remove page count in search result

--- images/skin.js
+++ images/skin.js
@@ -1,145 +1,146 @@
-var body = $('body'),
-	cover = $('#cover'),
-	nav = $('#nav'),
-	side = $('#side'),
-	search = $('#search'),
-	page = $('#page'),
-	botbtn = $('#bot-btn');
+(function ($) {
+    var $body = $('body'),
+        $cover = $('#cover'),
+        $nav = $('#nav'),
+        $side = $('#side'),
+        $search = $('#search'),
+        $page = $('#page'),
+        $botbtn = $('#bot-btn');
 
-function disableScroll(a) {
-	body.toggleClass('scroll-hidden',a);
-}
+    $body.scrollable = function (can) {
+        $body.toggleClass('scroll-hidden', !can);
+    };
 
-function nav_transparent() {
-	if (($(window).scrollTop() >= 100 && nav.hasClass('transparent'))
-		|| ($(window).scrollTop() < 100 && !nav.hasClass('transparent')))
-		nav.toggleClass('transparent');
-}
+    $nav.scrollHandler = function () {
+        if ($(window).scrollTop() >= 100 && $nav.hasClass('transparent')) {
+            $nav.toggleClass('transparent', false);
+        } else if ($(window).scrollTop() < 100 && !($nav.hasClass('transparent'))) {
+            $nav.toggleClass('transparent', true);
+        }
+    };
 
-function updateCover(a) {
-	disableScroll(false);
-	cover.toggleClass('active f', false);
-	side.toggleClass('active',false);
+    $(window).scroll($nav.scrollHandler);
+    $nav.scrollHandler();
 
-	switch(a) {
-	case 'side':
-		disableScroll(true);
-		cover.toggleClass('active',true);
-		side.toggleClass('active',true);
-		break;
-	case 'floating':
-		disableScroll(true);
-		cover.toggleClass('active f',true);
-		cover.find('.card.floating')
-			.html($('.floating-data.use').removeClass('use').clone().removeClass('floating-data'));
-		break;
-	default:
-		cover.find('.card.floating').html('');
-	}
-};
+    $cover.reset = function () {
+        $body.scrollable(true);
+        $cover.toggleClass('active f', false).find('.card.floating').html('');
+        $side.toggleClass('active', false);
 
-$(document).ready(function () {
-// nav
-	$(window).scroll(nav_transparent);
-	nav_transparent();
-// Nav menu
-	$('#nav .icon').click(function () {
-		if(!cover.hasClass('active'))
-			updateCover('side');
-		else
-			updateCover('off');
-	});
-	cover.click(function () {updateCover('off')});
+        return true;
+    };
+    $cover.active = function () {
+        $body.scrollable(false);
+        $cover.toggleClass('active', true);
 
-// Floating
-	$('.admin .fa-bars').click(function () {
-		if(!cover.hasClass('active')) {
-			$(this).parent().find('.floating-data').addClass('use');
-			updateCover('floating');
-		} else
-			updateCover('off');
-	});
-});
+        var tmp = $('.floating-data.use');
+        if (tmp) {
+            $cover.toggleClass('f', true);
+            $cover.find('.card.floating')
+                .html(tmp.removeClass('use').clone().removeClass('floating-data'));
+        }
 
-side.ready(function() {
-	side.find('li[class*="t_menu"]').parent().addClass('blogmenu');
-	side.find('a[href="/category"]').parent().parent().addClass('category');
-	$('.blogmenu a, .category a').addClass('item');
-	
-	$('.category > li > ul > li > ul').addClass('subcategory')
-		.find('a').addClass('subitem');
-});
+        return true;
+    };
 
+    $cover.click($cover.reset);
 
-// Page
-function f_page(a) {
-	var num = a.find('.num');
-	if(num.length <= 1) { a.remove(); return; }
-	else {
-		var selected = a.find('.selected'), parent = selected.parent();
-		parent.after(selected.addClass('btn num'));
-		parent.remove();
-	}
-}
+    $side.active = function (active) {
+        $side.toggleClass('active', active);
+        return active ? $cover.active() : $cover.reset();
+    };
 
-page.ready(function() {f_page(page)});
+    // toggle Side menu
+    $nav.find('.icon').click(function () {
+        return $side.active(!($cover.hasClass('active')));
+    });
 
-// Bottom button
-function buttom_btn_toggle() {
-	if (($(window).scrollTop() != 0 && botbtn.hasClass('hidden'))
-		|| $(window).scrollTop() == 0 && !botbtn.hasClass('hidden'))
-		botbtn.toggleClass('hidden');
-}
+    // toggle Admin floating menu
+    $('.admin .fa-bars').click(function () {
+        $(this).parent().find('.floating-data').addClass('use');
+        return $cover.active();
+    });
 
-botbtn.ready(function () {
-	var b_arrow_up = botbtn.find('.fa-arrow-up');
-	b_arrow_up.click(function () {$('html, body').animate({'scrollTop':'0'}, 250); return false;});
-	$(window).scroll(buttom_btn_toggle);
-	buttom_btn_toggle();
-});
+    // Side menu init
+    $side.ready(function () {
+        $side.find('li[class*="t_menu"]').parent().addClass('blogmenu');
+        $side.find('a[href="/category"]').parent().parent().addClass('category');
+        $('.blogmenu a, .category a').addClass('item');
 
-function t_search() {
-	var query = $('#query').val(),
-		url = blog_link + '/search/' + query;
-	var pathname = decodeURI($(location).attr('pathname').replace(/^\//,'')).split('/')[0];
-	
-	if(query == '')
-		return;
+        $('.category > li > ul > li > ul').addClass('subcategory')
+            .find('a').addClass('subitem');
+    });
 
-	if ((pathname == 'search') || (pathname == 'category') || (pathname == 'archive'))
-		$(location).attr('href',url);
-	else {
-		$.ajax({
-			url: url,
-			success: function(data, textStatus, jqXHR) {
-				var list = $(data).find('#search_list, #page').clone();
-				$('#search_list, #search_list + #page').remove();
-				f_page(list.filter('#page'));
-				if(list.find('.card-list a').length > 0) {
-					search.after(list);
-				}
-			}
-		});
-	}
-}
+    // Page init
+    $page.ready(function () {
+        if ($page.find('.num').length <= 1) {
+            $page.remove();
+        } else {
+            var selected = $page.find('.selected'), parent = selected.parent();
+            parent.after(selected.addClass('btn num selected'));
+            parent.remove();
+        }
+    });
 
-search.ready(function () {
-	$('#query').keypress(function (e) {(e.keyCode == 13) && t_search();});
-	$('#query ~ .button').click(t_search);
-});
+    $botbtn.find('.fa-arrow-up').click(function () {
+        $('html, body').animate({'scrollTop': '0'}, 250);
+    });
 
-function highlightSide() {
-	var pathname = decodeURI($(location).attr('pathname').replace(/^\//,'')).split('/');
-	
-	if(pathname[0] == 'category' && pathname[1]) {
-		var name = pathname[2] ? pathname[2] : pathname[1];
-		side.find('li a').each(function (index) {
-			if(name == $(this).text().replace(/\s\(\d+\)$/g,''))
-				$(this).addClass('accent');
-		});
-	} else {
-		side.find('li a[href="/'+pathname[0]+'"]').addClass('accent');
-	}
-}
+    $botbtn.scrollHandler = function () {
+        if ($(window).scrollTop() !== 0 && $botbtn.hasClass('hidden')) {
+            $botbtn.toggleClass('hidden', false);
+        } else if ($(window).scrollTop() === 0 && !($botbtn.hasClass('hidden'))) {
+            $botbtn.toggleClass('hidden', true);
+        }
+    };
 
-side.ready(highlightSide);
+    $(window).scroll($botbtn.scrollHandler);
+    $botbtn.scrollHandler();
+
+    $search.search = function () {
+        var query = $('#query').val(),
+            url = blog_link + '/search/' + query,
+            pathname = decodeURI($(location).attr('pathname').replace(/^\//, '')).split('/');
+
+        if (query === '') {
+            return false;
+        }
+
+        if ((pathname === 'search') || (pathname === 'category') || (pathname === 'archive')) {
+            $(location).attr('href', url);
+        } else {
+            $.ajax({
+                url: url,
+                success: function (data, a, b) {
+                    var $s_list = $(data).find('#search_list').clone(),
+                        $s_page = $(data).find('#page').clone();
+                    $('#search_list, #search_list ~ #page').remove();
+                    if ($s_list.find('.card-list a').length > 0) {
+                        $search.after($s_list, $s_page);
+                    }
+                }
+            });
+        }
+    };
+
+    $search.find('#query').keypress(function (e) {
+        (e.keyCode === 13) && $search.search();
+    });
+    $search.find('#query ~ .button').click($search.search);
+
+    $side.ready(function () {
+        var pathname = decodeURI($(location).attr('pathname').replace(/^\//, '')).split('/');
+    
+        if (pathname[0] === 'category' && pathname[1]) {
+            var name = pathname[2] ? pathname[2] : pathname[1];
+            $side.find('li a').each(function (index) {
+                if (name === $(this).text().replace(/\s\(\d+\)$/g, '')) {
+                    $(this).addClass('accent');
+                }
+            });
+        } else {
+            $side.find('li a[href="/' + pathname[0] + '"]').addClass('accent');
+        }
+    });
+    return true;
+}(jQuery));
