7c14415e4eeb4ec11c77ca5c215d7da33d263465
Inseop Kim <opnayn@gmail.com>
2017-01-27 11:18:10 +0000

    skin: Change '#bot-btn' to '.bot-btn'

--- images/skin.js
+++ images/skin.js
@@ -5,7 +5,7 @@
 		$side = $('#side'),
 		$search = $('#side .search'),
 		$page = $('.page'),
-		$botbtn = $('#bot-btn'),
+		$botbtn = $('.bot-btn'),
 		blog_link = $(location).attr('protocol') + "//" + $(location).attr('host');
 
 	$body.scrollable = function (can) {$body.toggleClass('scroll-hidden', !can);};
