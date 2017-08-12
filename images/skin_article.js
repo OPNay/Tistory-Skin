227db7650e02cbe67957ba4b0304183a32e34930
Kim In Seop <opnayn@gmail.com>
2017-08-12 14:14:49 +0000

    js: Remove unused skin_article.js

--- images/skin_article.js
+++
@@ -1,6 +0,0 @@
-$('.article').ready(function () {
-    // Image
-    var img_block = $(this).find('.imageblock');
-    img_block.removeAttr('style');
-    img_block.find('.cap1').attr('style','display: block;');
-});
