0a229b77418a6c4e4957a9337d438641c84185d9
Kim In Seop <opnayn@gmail.com>
2018-01-21 01:09:34 +0900

    js: window.resize: Fixed wide

--- images/window.resize.js
+++ images/window.resize.js
@@ -15,18 +15,19 @@
             if (prev_mode !== mode) {
                 a.removeClass('desktop tablet mobile')
                     .addClass(mode === 0 ? 'mobile' : (mode === 1 ? 'tablet' : 'desktop'));
-                if (mode === 2) {
-				    // side always show
-				    $('.side').toggleClass('active', false);
-                    $('.cover').remove();
-
-                    if ((width >= 1300) && !a.hasClass('wide')) {
-                        a.addClass('wide');
-                    } else if ((width < 1300) && a.hasClass('wide')) {
-                        a.removeClass('wide');
-                    }
-                }
                 prev_mode = mode;
+            }
+            
+            if (mode === 2) {
+                // side always show
+                $('.side').toggleClass('active', false);
+                $('.cover').remove();
+
+                if ((width >= 1300) && !a.hasClass('wide')) {
+                    a.addClass('wide');
+                } else if ((width < 1300) && a.hasClass('wide')) {
+                    a.removeClass('wide');
+                }
             }
 		};
 
