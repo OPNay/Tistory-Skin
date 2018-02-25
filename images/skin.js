c8f56755b4a81c740b9b5c9aa913d3832764a8d4
Kim In Seop <opnayn@gmail.com>
2018-02-26 03:05:22 +0900

    search button change to a tag

--- images/skin.js
+++ images/skin.js
@@ -25,9 +25,10 @@
 
 	$('.s_search .s_btn').on('click', function () {
 		html.toggleClass('func_search');
+		$('.s_input').focus();
 	});
 	
-	$('#s_post_search').on('keypress', function (e) {
+	$('.s_input').on('keypress', function (e) {
 		var query = $(this).val().trim();
 		if (e.keyCode === 13 && query !== '') {
 			location.href = encodeURI(location.origin + '/search/' + query);
